147c5a58555ae55873bc294cf438b39e8fa67c8d
Lucas Polo <pradopolo@gmail.com>
2018-10-01 20:02:56 -0300

    Criada estrutura para receber comando mkdir
    close #4

--- src/com/rtjvm/scala/oop/files/Directory.scala
+++ src/com/rtjvm/scala/oop/files/Directory.scala
@@ -2,6 +2,20 @@ package com.rtjvm.scala.oop.files
 
 class Directory(override val parentPath: String, override val name: String, val contents: List[DirEntry])
   extends DirEntry(parentPath, name) {
+  def replaceEntry(entryName: String, newEntry: Directory): Directory = ???
+
+  def findEntry(entryName: String): DirEntry = ???
+
+  def addEntry(newEntry: DirEntry): Directory = ???
+
+  def hasEntry(name: String): Boolean = ???
+
+  def getAllFoldersInPath: List[String] =
+    path.substring(1).split(Directory.SEPARATOR).toList
+
+  def findDescendant(path: List[String]): Directory = ???
+
+  override def asDirectory: Directory = this
 
 }
 
